05787e1fd4d86628ae7b11141617f0cc4fcc8e1c
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-01-31 20:29:38 +0530

    Implement mergeKLists for merging k sorted linked lists in C

---
+++ merge_k_sorted_array.c
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    struct ListNode dummy;
+    struct ListNode *tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    tail->next = l1 ? l1 : l2;
+    return dummy.next;
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if (listsSize == 0) return NULL;
+
+    struct ListNode* result = NULL;
+
+    for (int i = 0; i < listsSize; i++) {
+        result = mergeTwoLists(result, lists[i]);
+    }
+
+    return result;
+}
